db33cedf34cbac9e9f6ad0a9abcdb7973e356fa2
vignesh <vigneshinvicible1208@gmail.com>
2022-05-04 14:14:10 +0530

    initial comit

--- src/Components/MovieCard.js
+++ src/Components/MovieCard.js
@@ -4,26 +4,29 @@ function MovieCard(props) {
 
   const [like, setLike]=useState(0)
   const [dislike, setDislike]=useState(0)
+  const [toggle, setToggle] =useState(false)
 
   return (
 
     <>
        <div className="col-lg-4 py-3 ">
-            <div class=" h-100">
+            <div class=" bg-light h-100">
               <img
                 src={props.data.image}
                 class="card-img-top movie-img "
                 alt="movie-img"
               />
               <div class="card-body d-flex justify-content-between ">
-                <h5 class="card-title fs-3  ">{props.data.title}</h5>
+                <h5 class="card-title fs-3  ">{props.data.title} <button onClick={()=>setToggle(!toggle)} className="btn btn-outline"><i class="fa-solid fa-angle-down "></i></button> </h5>
                 <span className="rating "><i class="fa-solid fa-star rate"></i> {props.data.rating}  </span>
               </div>
-              <p class="card-text fs-"> {props.data.discription} </p>
+              {
+                toggle ?   <p class="card-text fs-5 px-3" > {props.data.discription} </p> : null
+              }
               <div >
                 
               <button onClick={()=>setLike(like+1)} className="px-3 mx-2">👍 {like}</button>
-              <button onClick={()=> setDislike(dislike+1)} className="px-3 mx-2">👎🏻 {dislike}</button>
+              <button onClick={()=> setDislike(dislike+1)} className="px-3 m-2">👎🏻 {dislike}</button>
               </div>
             </div>
           </div>
